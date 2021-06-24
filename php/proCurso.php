68ce50d1f9dbda22c44837dfc037454a8db1aab2
Jorge Alberto <heisenheim97@gmail.com>
2021-06-24 14:25:52 -0500

    Se optimiza correo

--- php/proCurso.php
+++ php/proCurso.php
@@ -1,6 +1,7 @@
 <?php
 include("../conexion/conexion.php");
-
+include_once('../php-mailer/class.phpmailer.php');
+include_once('../php-mailer/class.smtp.php');
 $opcion = $_POST["opcion"];
 $informacion = [];
 
@@ -33,9 +34,9 @@ foreach ($valor as $idinsps) {
 		}
 		if(enviarCorreo($idinsps,$id_mstr,$hcurso,$fcurso,$fechaf,$idinst,$sede,$modalidad,$link, $conexion))
 		{ 
-			echo "0";	
+//			echo "0";	
 		}else{	
-			echo "1";	
+//			echo "1";	
 		}
 	}
 }else if($opcion === 'actualizar'){
@@ -111,7 +112,7 @@ function actualizar($idinsp, $nombre, $apellidos, $correo, $idarea, $puesto,$uni
 
 function enviarCorreo($idinsps,$id_mstr,$hcurso,$fcurso,$fechaf,$idinst,$sede,$modalidad,$link, $conexion){
 
-		$query = "SELECT gstNombr,gstApell,gstCinst, gstCorro, gstTipo, modalidad, gstCargo, fcurso FROM personal INNER JOIN cursos	ON cursos.idinsp = personal.gstIdper
+		$query = "SELECT gstNombr,gstApell,gstCinst, gstCorro, gstTipo, modalidad, gstCargo, link, fcurso FROM personal INNER JOIN cursos	ON cursos.idinsp = personal.gstIdper
 					INNER JOIN listacursos ON cursos.idmstr = listacursos.gstIdlsc WHERE personal.gstIdper = $idinsps AND cursos.estado = 0";
 		$resultado= mysqli_query($conexion,$query);
 		$fila = mysqli_fetch_assoc($resultado);
@@ -124,32 +125,36 @@ function enviarCorreo($idinsps,$id_mstr,$hcurso,$fcurso,$fechaf,$idinst,$sede,$m
 		$cargo = $fila['gstCargo'];
 		$apellido = $fila['gstApell'];
 		$fcurso   = date("d-m-Y");
+		
+		
+		$mail = new PHPMailer();
+		$mail->IsSMTP();
+		$mail->SMTPAuth = true;
+		$mail->SMTPSecure = "ssl";
+		$mail->CharSet = "Content-Type: text/html; charset=utf-8";
+		$mail->Host = "smtp.gmail.com";
+		$mail->Port = 465;
+		$mail->Username ='jmondragonescamilla@gmail.com';
+		$mail->Password = 'ELVIS_wolf97';
 
-		$to = "jmondragonescamilla@gmail.com";
-		// $to = ($correo);
-		$subject = "NUEVO CURSO PROGRAMADO";
-		$headers = "MIME-Version: 1.0" . "\r\n";
-		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-		$message = "<center><img src='https://www.aeropuertodetoluca.com.mx/en/admin/images/iconos-autoridad/autoridad-aeronautica.png' width='320px;' alt='imagen de cabezera' disabled></center><table width='100%'><br>
-		<tr><td bgcolor='#00A7B5' align='center'><span style='font-size: 19px; color: white'>INSCRITO CON EXITO!</span></td></tr>
-		<tr><td style='text-align: center; font-size: 15px;'>Folio: ".$idinsps."</td></tr>
-		<tr><td style='text-align: center; font-size: 15px;'>Nombre del participante: ".$nombre." ".$apellido."</td></tr>
-		<tr><td style='text-align: center; font-size: 15px;'>Tipo de curso: ".$tipoCurso."</td></tr>
-		<tr><td style='text-align: center; font-size: 15px;'>Fecha Inicio: ".$fcurso."</td></tr>
-		<tr><td style='text-align: center; font-size: 15px;'>Hora: ".$hcurso." Hrs</td></tr>
-		<tr><td style='text-align: center; font-size: 15px;'>Cargo: ".$cargo."</td></tr>
-		<tr><td style='text-align: center; font-size: 15px;'>Sede del curso: ".$sede."</td></tr>
-		<tr><td style='text-align: center; font-size: 15px;'>Modalidad: ".$modalidad."</td></tr>
-		<hr><center>
-		<font color='#a1a1a1'>NOTA IMPORTANTE: Este correo se genera automaticamente. Por favor no responda o reenvie correos a de esta cuenta de e-mail.
-		</center><hr>
-		</table>";
-		mail($to, $subject, $message, $headers);
-		$envio = mail($to, $subject, $message, $headers);
-		if ($envio == true){ 
-			return true;		
-		}else{	
-			return false;	}
+		$mail->AddAddress('jmondragonescamilla@gmail.com');
+		$mail->Subject = "NUEVO CURSO PROGRAMADO";
+		$msg = "<center><img src='https://www.aeropuertodetoluca.com.mx/en/admin/images/iconos-autoridad/autoridad-aeronautica.png' width='320px;' alt='imagen de cabezera' disabled></center><table width='100%'><br>
+			<tr><td bgcolor='#00A7B5' align='center'><span style='font-size: 19px; color: white'>INSCRITO CON EXITO!</span></td></tr>
+			<tr><td style='text-align: center; font-size: 15px;'>Folio: ".$idinsps."</td></tr>
+			<tr><td style='text-align: center; font-size: 15px;'>Nombre del participante: ".$nombre." ".$apellido."</td></tr>
+			<tr><td style='text-align: center; font-size: 15px;'>Tipo de curso: ".$tipoCurso."</td></tr>
+			<tr><td style='text-align: center; font-size: 15px;'>Fecha Inicio: ".$fcurso."</td></tr>
+			<tr><td style='text-align: center; font-size: 15px;'>Hora: ".$hcurso." Hrs</td></tr>
+			<tr><td style='text-align: center; font-size: 15px;'>Cargo: ".$cargo."</td></tr>
+			<tr><td style='text-align: center; font-size: 15px;'>Sede del curso: ".$sede."</td></tr>
+			<tr><td style='text-align: center; font-size: 15px;'>Modalidad: ".$modalidad."</td></tr>
+			<hr><center>
+			<font color='#a1a1a1'>NOTA IMPORTANTE: Este correo se genera automaticamente. Por favor no responda o reenvie correos a de esta cuenta de e-mail.
+			</center><hr>
+			</table>";
+		$mail->MsgHTML($msg);
+		$mail->send();
 }
 function cerrar($conexion){
 
@@ -157,4 +162,5 @@ function cerrar($conexion){
 
 }
 ?>
+
 
