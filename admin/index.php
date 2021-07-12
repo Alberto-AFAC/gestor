e91c82c568671e012ddb88d79ab35059ac3941d5
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-07-12 17:51:41 -0500

    Ganntt

--- admin/index.php
+++ admin/index.php
@@ -27,6 +27,11 @@
   <link rel="stylesheet" href="../bower_components/bootstrap-daterangepicker/daterangepicker.css">
   <!-- bootstrap wysihtml5 - text editor -->
   <link rel="stylesheet" href="../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link href="https://playground.anychart.com/gallery/src/Gantt_Charts/Planned-vs-Actual_Chart/iframe" rel="canonical">
+  <!-- <link href="https://cdn.anychart.com/releases/v8/css/anychart-ui.min.css" rel="stylesheet" type="text/css">
+  <link href="https://cdn.anychart.com/releases/v8/fonts/css/anychart-font.min.css" rel="stylesheet" type="text/css"> -->
+  <link href="https://cdn.anychart.com/releases/8.10.0/css/anychart-ui.min.css?hcode=a0c21fc77e1449cc86299c5faa067dc4"
+        rel="stylesheet" type="text/css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -196,10 +201,15 @@ include('header.php');
                   <span class="progress-description">
                   <a link rel="stylesheet" href="lisCurso.php" style = "color:white" class="small-box-footer">information <i class="fa fa-arrow-circle-right"></i></a>
             </div>
+           
             <!-- /.info-box-content -->
         
     </section>
-    
+    <div class="container">
+    <!-- <div style="margin: 0; padding: 0; width: 100%; height: 100%;" id="container"></div> -->
+    <div style="margin: 0; padding: 0; width: 100%; height: 100%;" id="container"></div>
+
+    </div>
     <!-- /.content -->
   </div>
 
@@ -235,7 +245,7 @@ include('header.php');
 <script src="../bower_components/raphael/raphael.min.js"></script>
 <script src="../bower_components/morris.js/morris.min.js"></script>
 <!-- Sparkline -->
-<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- <script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script> -->
 <!-- jvectormap -->
 <script src="../plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
 <script src="../plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
@@ -259,6 +269,227 @@ include('header.php');
 <!-- AdminLTE for demo purposes -->
 <script src="../dist/js/demo.js"></script>
 <script src="../js/status.js"></script>
+<script
+        src="https://cdn.anychart.com/releases/8.10.0/js/anychart-base.min.js?hcode=a0c21fc77e1449cc86299c5faa067dc4">
+    </script>
+    <script
+        src="https://cdn.anychart.com/releases/8.10.0/js/anychart-gantt.min.js?hcode=a0c21fc77e1449cc86299c5faa067dc4">
+    </script>
+    <script
+        src="https://cdn.anychart.com/releases/8.10.0/js/anychart-exports.min.js?hcode=a0c21fc77e1449cc86299c5faa067dc4">
+    </script>
+    <script src="https://cdn.anychart.com/releases/8.10.0/js/anychart-ui.min.js?hcode=a0c21fc77e1449cc86299c5faa067dc4">
+    </script>
+    <script type="text/javascript">
+    anychart.onDocumentReady(function () {
+
+// create data
+var data = [
+  {
+    id: "1",
+    name: "BASICO",
+    start_date: "2018-01-15",
+    end_date: "2018-03-10",
+    child_items: [
+      {
+        id: "1_1",
+        name: "INSPECTOR AERONAUTICO",
+        start_date: "2018-01-15",
+        end_date: "2018-01-25"
+      },
+      {
+        id: "1_2",
+        name: "INSPECTOR AERONAUTICO",
+        start_date: "2018-01-20",
+        end_date: "2018-02-04"
+      },
+      {
+        id: "1_3",
+        name: "INSPECTOR AERONAUTICO",
+        start_date: "2018-02-05",
+        actualEnd: "2018-02-05"
+      },
+      {
+        id: "1_4",
+        name: "INSPECTOR AERONAUTICO",
+        start_date: "2018-02-05",
+        end_date: "2018-02-24"
+      },
+      {
+        id: "1_5",
+        name: "INSPECTOR AERONAUTICO",
+        start_date: "2018-02-25",
+        end_date: "2018-03-10"
+      }
+  ]}
+];
+
+// create a data tree
+var treeData = anychart.data.tree(data, "as-tree", null, {children: "child_items"});
+
+// map the data
+var mapping = treeData.mapAs({actualStart: "start_date", actualEnd: "end_date"});
+
+// create a chart
+var chart = anychart.ganttProject();
+
+// set the data
+chart.data(mapping);
+
+// configure the scale
+chart.getTimeline().scale().maximum("2018-03-15");
+
+// set the container id
+chart.container("container");
+
+// initiate drawing the chart
+chart.draw();
+
+// fit elements to the width of the timeline
+chart.fitAll();
+});
+    </script>
+<!-- <script src="js/anychart-base.min.js"></script>
+  <script src="js/anychart-ui.min.js"></script>
+  <script src="js/anychart-exports.min.js"></script>
+  <script src="js/anychart-gantt.min.js"></script>
+  <script src="js/anychart-data-adapter.min.js"></script>
+  <script type="text/javascript">anychart.onDocumentReady(function () {
+  // The data used in this sample can be obtained from the CDN
+  // https://cdn.anychart.com/samples/gantt-charts/server-status-list/data.json
+  anychart.data.loadJsonFile(
+    'http://localhost/gestor/php/lisCurso.php',
+    function (data) {
+      // create data tree on our data
+      var treeData = anychart.data.tree(data, 'as-table');
+
+      // create project gantt chart
+      var chart = anychart.ganttResource();
+
+      // set data for the chart
+      chart.data(treeData);
+
+      // set start splitter position settings
+      chart.splitterPosition(320);
+
+      // get chart data grid link to set column settings
+      var dataGrid = chart.dataGrid();
+
+      // hide first column
+      dataGrid.column(0).enabled(false);
+
+      // get chart timeline
+      var timeLine = chart.getTimeline();
+      // set base fill
+      timeLine.elements().fill(function () {
+        // get status from data item
+        var status = this.period.status;
+
+        // create fill object
+        var fill = {
+          // if this element has children, then add opacity to it
+          opacity: this.item.numChildren() ? 1 : 0.6
+        };
+
+        // set fill color by status
+        switch (status) {
+          case 'online':
+            fill.color = 'green';
+            break;
+          case 'maintenance':
+            fill.color = 'orange';
+            break;
+          case 'offline':
+            fill.color = 'red';
+            break;
+          default:
+        }
+
+        return fill;
+      });
+
+      // set base stroke
+      timeLine.elements().stroke('none');
+      // set select fill
+      timeLine.elements().selected().fill('#ef6c00');
+
+      // set first column settings
+      var firstColumn = dataGrid.column(1);
+      firstColumn.labels().hAlign('left');
+      firstColumn
+        .title('CURSO')
+        .width(140)
+        .labelsOverrider(labelTextSettingsOverrider)
+        .labels()
+        .format(function () {
+          return this.item.get('gstTitlo') || '';
+        });
+
+      // set first column settings
+      var secondColumn = dataGrid.column(2);
+      secondColumn.labels().hAlign('right');
+      secondColumn
+        .title('Sede')
+        .width(60)
+        .labelsOverrider(labelTextSettingsOverrider)
+        .labels()
+        .format(function () {
+          return this.item.get('sede') || '';
+        });
+
+      // set first column settings
+      var thirdColumn = dataGrid.column(3);
+      thirdColumn.labels().hAlign('right');
+      thirdColumn
+        .title('Maintenance')
+        .width(60)
+        .labelsOverrider(labelTextSettingsOverrider)
+        .labels()
+        .format(function () {
+          return this.item.get('maintenance') || '';
+        });
+
+      // set first column settings
+      var fourthColumn = dataGrid.column(4);
+      fourthColumn.labels().hAlign('right');
+      fourthColumn
+        .title('Offline')
+        .width(60)
+        .labelsOverrider(labelTextSettingsOverrider)
+        .labels()
+        .format(function () {
+          return this.item.get('offline') || '';
+        });
+
+      // set container id for the chart
+      chart.container('container');
+
+      // initiate chart drawing
+      chart.draw();
+
+      // zoom chart to specified date
+      chart.zoomTo(
+        Date.UTC(2008, 0, 31, 1, 36),
+        Date.UTC(2008, 1, 15, 10, 3)
+      );
+    }
+  );
+});
+
+function labelTextSettingsOverrider(label, item) {
+  switch (item.get('status')) {
+    case 'online':
+      label.fontColor('green').fontWeight('bold');
+      break;
+    case 'maintenance':
+      label.fontColor('orange').fontWeight('bold');
+      break;
+    case 'offline':
+      label.fontColor('red').fontWeight('bold');
+      break;
+    default:
+  }
+}</script> -->
 </body>
 </html>
 
