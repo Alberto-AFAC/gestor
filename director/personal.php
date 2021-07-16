17d7d395795e12b35f8bc179cecdad5e929d3555
jessicasoto01afac <jessica.soto928@gmail.com>
2021-07-15 22:24:20 -0500

    icono y titulo

--- director/personal.php
+++ director/personal.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Gestor inspectores|Personal</title>
+  <title>Gestor inspectores | Personal</title>
   <link rel="shortcut icon" href="../dist/img/iconafac.ico" />
    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
