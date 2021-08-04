7fef14418a72c278845085b460c09d60ff003c28
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-08-04 17:47:49 -0500

    Se incorpora menu en perfil humanos

--- humanos/header.php
+++ humanos/header.php
@@ -27,7 +27,7 @@ unset($_SESSION['consulta']);
 <link rel="stylesheet" type="text/css" href="../css/style.css">
 <header class="main-header">
     <!-- Logo -->
-    <a href="./" class="logo">
+    <a href="humanos.php" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>G</b>DI</span>
       <!-- logo for regular state and mobile devices -->
@@ -153,7 +153,7 @@ unset($_SESSION['consulta']);
         </li>-->
 
         <li class="active">
-          <a href="./">
+          <a href="humanos.php">
             <i class="fa ion-android-plane"></i> <span>Dashboard</span>
             <span class="pull-right-container">
             </span>
