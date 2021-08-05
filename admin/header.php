cd9cee3bc1e2488e310a5f8ddfb7c4fb65096466
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-08-05 11:44:56 -0500

    Gantt change position

--- admin/header.php
+++ admin/header.php
@@ -181,7 +181,6 @@
             <li><a href="conCursos.php"><i class="fa fa-search"></i> Catálogos de cursos</a></li>
             <li><a href="programa.php"><i class="fa ion-compose"></i> Programación del Curso</a></li>
             <li><a href="lisCurso.php"><i class="fa ion-compose"></i> Cursos Programados</a></li>
-            <li><a href="cursosgantt.php"><i class="fa fa-area-chart"></i> Gantt Cursos programados</a></li>
             <!-- <li><a href="estadisticas.php"><i class="fa fa-pie-chart"></i> Estadisticas Generales</a></li> -->
             <li><a href="niveldesatis.php"><i class="fa fa-line-chart"></i>Nivel de satisfacción</a></li>
           </ul>
@@ -191,6 +190,15 @@
         <li>
           <a href="calendar/calendar.php">
             <i class="fa fa-calendar"></i> <span>Calendario</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-red"></small>
+              <small class="label pull-right bg-blue"></small>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="cursosgantt.php">
+          <i class="fa fa-area-chart"></i> <span>Gantt Cursos programados</span>
             <span class="pull-right-container">
               <small class="label pull-right bg-red"></small>
               <small class="label pull-right bg-blue"></small>
