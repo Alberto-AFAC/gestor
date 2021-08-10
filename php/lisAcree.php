64402b4b9f86f9c5ff934ab9d7bf915d3a1e2305
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-08-10 11:27:01 -0500

    Cursos Acreeditados

---
+++ php/lisAcree.php
@@ -0,0 +1,28 @@
+<?php
+	include("../conexion/conexion.php");
+	session_start();
+	
+	$query = "SELECT *,COUNT(*) as prtcpnts FROM cursos INNER JOIN listacursos ON listacursos.gstIdlsc = cursos.idmstr WHERE proceso = 'Finalizado' GROUP by cursos.idmstr,cursos.idinst ORDER BY id_curso DESC ";
+	$resultado = mysqli_query($conexion, $query);
+
+	if(!$resultado){
+		die("error");
+	}else{
+		while($data = mysqli_fetch_assoc($resultado)){
+
+			$arreglo["data"][] = $data; 
+		}
+		if(isset($arreglo)&&!empty($arreglo)){
+
+			echo json_encode($arreglo);
+		}else{
+
+			echo $arreglo='0';
+		}
+	}
+		mysqli_free_result($resultado);
+		mysqli_close($conexion);
+
+?>
+
+
