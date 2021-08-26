1ee2647f6bb31a7327f04fc5fc9e737019095b73
Alberto-Escamilla <l161130082@gamadero.tecnm.mx>
2021-08-26 14:56:36 -0500

    Lista evaluada

--- php/curLista.php
+++ php/curLista.php
@@ -1,30 +1,55 @@
 <?php
-	include("../conexion/conexion.php");
-	session_start();
-	
-	$query = "SELECT * FROM cursos 
-			  INNER JOIN listacursos ON listacursos.gstIdlsc = cursos.idmstr 
-			  INNER JOIN personal ON idinsp = gstIdper 
-			  INNER JOIN categorias ON categorias.gstIdcat = personal.gstIDCat
-			  WHERE cursos.estado=0";
-	$resultado = mysqli_query($conexion, $query);
+    include("../conexion/conexion.php");
 
-	if(!$resultado){
-		die("error");
-	}else{
-		while($data = mysqli_fetch_assoc($resultado)){
 
-			$arreglo["data"][] = $data; 
-		}
-		if(isset($arreglo)&&!empty($arreglo)){
+$query = "
+SELECT * FROM cursos 
+              INNER JOIN listacursos ON listacursos.gstIdlsc = cursos.idmstr 
+              INNER JOIN personal ON idinsp = gstIdper 
+              INNER JOIN categorias ON categorias.gstIdcat = personal.gstIDCat
+              WHERE cursos.estado=0";
 
-			echo json_encode($arreglo);
-		}else{
+$resultado = mysqli_query($conexion, $query);
 
-			echo $arreglo='0';
-		}
-	}
-		mysqli_free_result($resultado);
-		mysqli_close($conexion);
+if(!$resultado){
+        die("error");
+    }else{
+
+while($data = mysqli_fetch_array($resultado)){ 
+$id_curso = $data['id_curso'];
+
+$queri = "
+SELECT * FROM reaccion WHERE id_curso = $id_curso AND estado = 0 ORDER BY id_curso DESC";
+$resul = mysqli_query($conexion, $queri);
+
+if($res = mysqli_fetch_array($resul)){
+// echo '<br>';
+// echo 'reacción'.$res[0];
+// echo'<br>';
+// echo 'esta'.$id_curso;
+// echo '<br>';
+$data["reaccion"] = 'SI EXISTE';
+$arreglo["data"][] = $data; 
+
+}else{
+
+// echo 'no esta'.$id_curso;
+$data["reaccion"] = 'NO EXISTE';
+$arreglo["data"][] = $data; 
+
+}
+
+}
+        if(isset($arreglo)&&!empty($arreglo)){
+
+            echo json_encode($arreglo);
+        }else{
+
+            echo $arreglo='0';
+        }
+    }
+        mysqli_free_result($resultado);
+        mysqli_close($conexion);
+
 
 ?>
