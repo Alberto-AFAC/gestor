2b1b51dc419eb1a5b74a25cb20e6dc61775cfb57
Alberto-Escamilla <l161130082@gamadero.tecnm.mx>
2021-09-13 17:53:22 -0500

    Se limpia código

--- inspector/constancia.php
+++ inspector/constancia.php
@@ -1,67 +1,5 @@
 <?php ob_start();
-    ini_set('date.timezone','America/Mexico_City');
-    include('../conexion/conexion.php');
-    $datos = $_GET['data'];
-    $query = "SELECT id, id_persona, id_codigocurso, personal.gstNombr, personal.gstApell, gstTitlo ,fcurso, YEAR(fechaf) AS ano, gstDrcin, cursos.evaluacion, DAY(fcurso) AS dia, 	MONTH(fcurso) AS MES, DAY(fechaf) AS diafinal, MONTH(fechaf) AS mesfinal, cursos.modalidad, CASE WHEN MONTH ( fcurso ) = 1 THEN
-    'enero' 
-    WHEN MONTH ( fcurso ) = 2 THEN
-    'febrero' 
-    WHEN MONTH ( fcurso ) = 3 THEN
-    'marzo' 
-    WHEN MONTH ( fcurso ) = 4 THEN
-    'abril' 
-    WHEN MONTH ( fcurso ) = 5 THEN
-    'mayo' 
-    WHEN MONTH ( fcurso ) = 6 THEN
-    'junio' 
-    WHEN MONTH ( fcurso ) = 7 THEN
-    'julio' 
-    WHEN MONTH ( fcurso ) = 8 THEN
-    'agosto' 
-    WHEN MONTH ( fcurso ) = 9 THEN
-    'septiembre' 
-    WHEN MONTH ( fcurso ) = 10 THEN
-    'octubre' 
-    WHEN MONTH ( fcurso ) = 11 THEN
-    'noviembre' 
-    WHEN MONTH ( fcurso ) = 12 THEN
-    'diciembre' ELSE 'novalido' END AS mesnombre,
-    CASE
-		WHEN MONTH ( fechaf ) = 1 THEN
-		'enero' 
-		WHEN MONTH ( fechaf ) = 2 THEN
-		'febrero' 
-		WHEN MONTH ( fechaf ) = 3 THEN
-		'marzo' 
-		WHEN MONTH ( fechaf ) = 4 THEN
-		'abril' 
-		WHEN MONTH ( fechaf ) = 5 THEN
-		'mayo' 
-		WHEN MONTH ( fechaf ) = 6 THEN
-		'junio' 
-		WHEN MONTH ( fechaf ) = 7 THEN
-		'julio' 
-		WHEN MONTH ( fechaf ) = 8 THEN
-		'agosto' 
-		WHEN MONTH ( fechaf ) = 9 THEN
-		'septiembre' 
-		WHEN MONTH ( fechaf ) = 10 THEN
-		'octubre' 
-		WHEN MONTH ( fechaf ) = 11 THEN
-		'noviembre' 
-		WHEN MONTH ( fechaf ) = 12 THEN
-		'diciembre' ELSE 'MES NO VALIDO' 
-	END AS mesfinales FROM constancias INNER JOIN personal ON personal.gstIdper = constancias.id_persona INNER JOIN cursos ON id_codigocurso = codigo INNER JOIN listacursos ON idmstr = gstIdlsc
-    WHERE id = $datos";
-    $const = mysqli_query($conexion, $query);
-    $con = mysqli_fetch_array($const);
-    $dia = array("cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve","diez","once","doce","trece","catorce","quince", "dieciseis","diecisiete","dieciocho","diecinueve", "veinte","veintiuno","veintidós","veintitres","veinticuatro","veinticinco","veintiseis","veintisiete","veintiocho","veintinueve","treinta","treinta y uno");
-    $fecha2= $dia[date('d')];
-    setlocale(LC_TIME, "spanish");
-    $mesactual = strftime("%B");
-    
-
-
+include('generating.php');
 ?>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -129,7 +67,7 @@
 <p class="titulo">Certificado</p>
 <p class="people">A : <span style="font-size: 60px; font-family: 'Montserrat', sans-serif; font-weight: bold; color: #BE0202;"><?php echo $con['gstNombr']." ".$con['gstApell'] ?></span></p>
 <div style="text-align: center;">
-    <span style="line-height:10px;" class="p-2">Por haber participado en el: </span>
+    <span style="line-height:10px;" class="p-2">Por haber participado en el curso: </span>
     <p class="titulo"><?php echo $con['gstTitlo']?></p>
     <span class="p-2">Impartido del <?php echo $con['dia']?> de <?php echo $con['mesnombre']?> al <?php echo $con['diafinal']?> de <?php echo $con['mesfinales']?> del <?php echo $con['ano']?>, en la modalidad <?php echo $con['modalidad']?>, con un total de <?php echo $con['gstDrcin']?> horas, obteniendo una calificación de <?php echo $con['evaluacion']?>/100</span><br>
     <span style="font-size: 22px; font-style: italic;" class="p-2">La presente se extiende a los <?php echo $fecha2?> dias del mes de <?php echo $mesactual ?> de dos mil veintiuno.</span>
