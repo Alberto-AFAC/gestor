754ccf88885535b62802219c156cafb5a2b961f6
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-09-24 14:57:09 -0500

    PRUEBAS

--- php/consdaTable.php
+++ php/consdaTable.php
@@ -2,10 +2,12 @@
 	include("../conexion/conexion.php");
     header('Content-Type: application/json');
 	session_start();
-	
+
+
+
 	$query = "SELECT * FROM listacursos 
 	WHERE estado = 0
-	ORDER BY gstIdlsc DESC";
+	GROUP BY gstIdlsc DESC";
 	$resultado = mysqli_query($conexion, $query);
 
 	if(!$resultado){
@@ -25,7 +27,27 @@ if($data["gstVignc"] == 101){
 if($data['gstTmrio'] == '0'){
     $temario = 'N/A';
 } else {
-    $temario = "<a href='{$data['gstTmrio']}' target='_blanck'><img src='../dist/img/pdf.svg' alt='PDF' width='30px;' cursor: pointer;' ></a>";
+    $temario = "<img data-toggle='modal' data-target='#exampleModal{$data['gstIdlsc']}' src='../dist/img/pdf.svg' alt='PDF' width='30px;' cursor: pointer;'>
+	<div class='modal fade' id='exampleModal{$data['gstIdlsc']}' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+  <div class='modal-dialog' role='document'>
+    <div class='modal-content'>
+      <div class='modal-header'>
+	  <h5 style='font-size: 28px;' class='modal-title col-11 text-center'><span style='font-weight: bold;'>TEMARIO</span><br>{$data['gstTitlo']}</h5>
+      </div>
+      <div class='modal-body'>
+	  <div class='jumbotron'>
+	  <div class='container'>
+	  <p class='lead'>AQUI VA EL NOMBRE</p>
+	  </div>
+	  <hr class='my-4'>
+	</div>
+      </div>
+      <div class='modal-footer'>
+        <button type='button' class='btn btn-primary' data-dismiss='modal'>CERRAR</button>
+      </div>
+    </div>
+  </div>
+</div>";
 }
 
 
@@ -33,7 +55,6 @@ if($data['gstTmrio'] == '0'){
 	 $caledario[] = [ $data["gstIdlsc"],$data["gstTitlo"],$data["gstTipo"],$data["gstPrfil"],$data["gstDrcin"],$data["gstCntnc"],$vigencia,$temario];
 
 		}
-
 
 
 		// if(isset($arreglo)&&!empty($arreglo)){
