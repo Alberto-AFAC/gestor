123fe46bbd0c44f2c5d69c0542e5844ee003e00a
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-09-28 16:39:19 -0500

    CONSTANCIAS

--- inspector/constancia.php
+++ inspector/constancia.php
@@ -260,38 +260,40 @@ if($con['gstCntnc'] == 'CONSTANCIA'){
 </div>";
 } else if($con['gstCntnc'] == 'CERTIFICADO'){
     echo "<div style='text-align: center;'>
-    <p class='CIAAC'>El Centro Internacional de Adiestramiento de Aviación Civil</p>
-    <p class='otorga'>Otorga el presente</p>
-    <p class='titulo-certificado'>{$con['gstCntnc']}</p>
+    <p class='CIAAC'>El Centro Internacional de Adiestramiento de Aviación Civil</p><br><br>
+    <p class='otorga'>Otorga el presente</p><br><br>
+    <p class='titulo-certificado'>{$con['gstCntnc']}</p><br><br><br>
     <p class='nombre-persona'>Al C:. <span
-    class='nombre-persona-c'>{$nombresCompletos}</span></p>
-    <p class='otorga'>Por haber participado en el curso:</p>
-            <p class='nombre-curso'>{$con['gstTitlo']}</p> 
-    </p><span class='p-2'>Comprendido durante el periódo del {$con['dia']} de {$con['mesnombre']}
-    al {$con['diafinal']} de {$con['mesfinales']} del presente año, en la modalidad <span class='p-2' style='font-weight:bold;'>{$con['modalidad']}</span> impartido por el <span
-        class='p-2' style='font-weight:bold;'>{$con['sede']}</span> con una duración de {$con['gstDrcin']}<br><span style='padding-top: 80px;' class='p-2'>Ciudad de México, a
-        {$hoy}</span>
-        <p class='p-2'>Director del CIAAC:</p></div>
-        <div style='padding-top: 3px; text-align: center;'>
-        <div class='row'>
-            <div class='column left'>
+    class='nombre-persona-c'>{$nombresCompletos}</span></p><br><br><br>
+    <p class='otorga'>Por haber participado en el curso:</p><br><br><br>
+            <p class='nombre-curso'>{$con['gstTitlo']}</p><br><br><br>
+            </p><span class='p-2'>Comprendido durante el periódo del {$con['dia']} de {$con['mesnombre']}
+            al {$con['diafinal']} de {$con['mesfinales']} del presente año, en la modalidad <span class='p-2' style='font-weight:bold;'>{$con['modalidad']}</span> impartido por el <span
+                class='p-2' style='font-weight:bold;'>{$con['sede']}</span> con una duración de {$con['gstDrcin']}<br><span style='padding-top: 80px;' class='p-2'>Ciudad de México, a
+                {$hoy}</span>
+                <p class='p-2'>Director del CIAAC:</p></div>
+                <div style='padding-top: 80%; text-align: center;'>
+                <div class='row'>
+                    <div class='column left'>
+                    </div>
+                    <div class='column middle'>
+                        <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
+                            class='p-2'>Secretaria de
+                            Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
+                            Adiestramiento de
+                            Aviación Civil / SCT-AFAC-CIAAC</span><br>
+                        <span style='font-weight: bold;' class='p-2'>Benjamín Romero Fuentes</span><br>
+                        <span style='font-weight: bold; line-height:22px;' class='p-2'>Gral. de División P.A. DEMA en
+                            Ret.</span>
+                    </div>
+                    <div class='column right'>
+                    <img style='float: right; width: 35%;' src='{$filename}'/>
+                    </div>
+                </div>
             </div>
-            <div class='column middle'>
-                <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
-                    class='p-2'>Secretaria de
-                    Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
-                    Adiestramiento de
-                    Aviación Civil / SCT-AFAC-CIAAC</span><br>
-                <span style='font-weight: bold;' class='p-2'>Benjamín Romero Fuentes</span><br>
-                <span style='font-weight: bold; line-height:22px;' class='p-2'>Gral. de División P.A. DEMA en
-                    Ret.</span>
-            </div>
-            <div class='column right'>
-            <img style='float: right; width: 35%;' src='{$filename}'/>
-            </div>
-        </div>
-    </div>
-    <div>
+            <div>
+<div style='page-break-after:always;'>
+</div>
     <p class='p-2'>Este certificado ampara los temas visto en el <span style='font-weight: bold;'>CURSO:
            {$con['gstTitlo']}</span>, que a
         continuación se enlistan:</p>
@@ -304,8 +306,11 @@ if($con['gstCntnc'] == 'CONSTANCIA'){
     Aviación Civil / SCT-AFAC-CIAAC</span>
     </div>
     <div class='footer-constancia'>
-    <span class='p-2'><span style='font-weight:bold;'>Lic. Rebeca Morales Reyes</span><br>Subdirectora de Diseño Pedagógico de Programas Aeronáuticos</span>
-</div>";
+    <span class='p-2' style='font-weight:bold;'>Lic. Rebeca Morales Reyes</span><br>Subdirectora de Diseño Pedagógico de Programas Aeronáuticos</span>
+    </div>
+
+
+    ";
 }else {
     echo "<div style='text-align: center;'>
     <p class='CIAAC'>El Centro Internacional de Adiestramiento de Aviación Civil</p><br><br>
@@ -363,7 +368,7 @@ if($con['gstCntnc'] == 'CONSTANCIA'){
        if($con['gstCntnc'] == 'CONSTANCIA'){
         echo "<p class='p-2'>{$temario}</p>";
        }else if($con['gstCntnc'] == 'CERTIFICADO'){
-        echo "<p class='p-2'>{$temario}</p>";
+        echo "<br><br><p class='p-2'>{$temario}</p>";
        }else {
            echo "";
        }
