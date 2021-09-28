8d7de21fe21f147cd6f723f9659bef2fb87e092d
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-09-28 14:56:57 -0500

    siguientes cambios

--- inspector/constancia.php
+++ inspector/constancia.php
@@ -1,60 +1,382 @@
-<?php ob_start(); ?>
-<h2>Reporte de Profesionales</h2>
- <br>  
+<?php ob_start(); 
+include('generating.php');
+require '../dist/phpqrcode/qrlib.php';
+$dir = 'temp/';
+
+if(!file_exists($dir))
+    mkdir($dir);
+$filename = $dir.'test.png';
+
+$tamanio = 5;
+$level = 'H';
+$frameSize = 1;
+// $contenido = $nombre . "FECHA DE SALIDA"  . $data['fecha_salida'] . "AEROLINEA" . $data['aerolinea'];
+$contenido = "INSTITUCIÓN: CENTRO INTERNACIONAL DE AVIACIÓN CIVIL, OTORGÓ A:". " " .$nombre. " " .$apellido. " " ."UN CERTIFICADO POR HABER PARTICIPADO EN EL CURSO CON FOLIO". " " .$registro. " ". "El DIA"." ".$dateFinal;
+
+QRcode::png($contenido, $filename, $level, $tamanio, $frameSize);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Certficate</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,800;1,700&display=swap"
+        rel="stylesheet">
+    <!-- SIN LIBRERIAS -->
+    <style>
+    .CIAAC {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 38px;
+    }
+
+    .otorga {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 26px;
+        /* padding-top: 50px;  */
+    }
+
+    p {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 35px;
+    }
+
+    .temario {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 25px;
+    }
+
+    .p-2 {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 24px;
+    }
+    .p-3 {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 20px;
+    }
+
+    .nombre-persona {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 40px;
+        text-align: center;
+    }
+
+    .nombre-persona-c {
+        font-size: 60px;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: bold;
+        color: #BE0202;
+    }
+
+    .titulo-certificador {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 40px;
+        font-weight: bold;
+        letter-spacing: 5px;
+        text-align: center;
+        text-transform: uppercase;
+    }
+    .titulo-certificador {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 35px;
+        font-weight: bold;
+        letter-spacing: 5px;
+        text-align: center;
+        text-transform: uppercase;
+    }
+
+    .nombre-curso {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 44px;
+        font-weight: bold;
+        letter-spacing: 5px;
+        text-align: center;
+        text-transform: uppercase;
+    }
+    .nombre-cursor {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 35px;
+        font-weight: bold;
+        letter-spacing: 5px;
+        text-align: center;
+        text-transform: uppercase;
+    }
+
+    #watermark {
+        position: fixed;
+        bottom: 5cm;
+        left: 5.5cm;
+        width: 16cm;
+        height: 9cm;
+        z-index: -1000;
+    }
+
+    .footer2 {
+        position: fixed;
+        bottom: 120px;
+        left: 80px;
+        right: 40px;
+        height: 50px;
+        text-align: left;
+        line-height: 35px;
+    }
+    .footer-constancia-gold {
+        position: fixed;
+        bottom: 195px;
+        left: 80px;
+        right: 40px;
+        height: 50px;
+        text-align: center;
+        line-height: 35px;
+    }
+    .afojas1 {
+        position: fixed;
+        bottom: 300px;
+        left: 80px;
+        font-size: 20px;
+        right: 40px;
+        height: 50px;
+        text-align: left;
+        line-height: 35px;
+        font-family: 'Montserrat', sans-serif;
+
+    }
+    .footer-constancia {
+        position: fixed;
+        bottom: 160px;
+        left: 80px;
+        font-size: 20px;
+        right: 40px;
+        height: 50px;
+        text-align: center;
+        line-height: 35px;
+        font-family: 'Montserrat', sans-serif;
+
+    }
+    footer {
+        position: fixed;
+        bottom: -40px;
+        left: 0px;
+        right: 0px;
+        height: 50px;
+        text-align: center;
+        line-height: 35px;
+    }
+
+        {
+        box-sizing: border-box;
+    }
+
+    /* Set additional styling options for the columns */
+    .column {
+        float: left;
+    }
+
+    /* Set width length for the left, right and middle columns */
+    .left {
+        width: 20%;
+    }
+
+    .middle {
+        width: 60%;
+    }
+
+    .right {
+        width: 20%;
+    }
+
+    .row:after {
+        content: "";
+        display: table;
+        clear: both;
+    }
+    </style>
+</head>
+
+<body>
+<?php
+            $path = '../dist/img/header.png';
+            $type = pathinfo($path, PATHINFO_EXTENSION);
+            $data = file_get_contents($path);
+            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        ?>
+    <div id="watermark">
+        <img src="../dist/img/back-image.jpg" height="95%" width="95%" />
+    </div>
+    <img src="<?php echo $base64 ?>" width="95%" height="113%" />
+    <?php
+if($con['gstCntnc'] == 'CONSTANCIA'){
+    echo "<div style='text-align: center;'>
+    <p class='CIAAC'>El Centro Internacional de Adiestramiento de Aviación Civil</p>
+    <p class='otorga'>Otorga el presente</p>
+    <p class='titulo-certificado'>{$con['gstCntnc']}</p>
+    <p class='nombre-persona'>Al C:. <span
+    class='nombre-persona-c'>{$nombresCompletos}</span></p>
+    <p class='otorga'>Por haber participado en el curso:</p>
+            <p class='nombre-curso'>{$con['gstTitlo']}</p> 
+    </p><span class='p-2'>Impartido por el <span style='font-weight:bold;'>{$con['sede']}</span><span class='p-2'>, comprendido durante el periódo del {$con['dia']} de {$con['mesnombre']}
+    al {$con['diafinal']} de {$con['mesfinales']} del presente año, con una duración de {$con['gstDrcin']}<br><span style='padding-top: 80px;' class='p-2'>Ciudad de México, a
+        {$hoy}</span>
+        <p class='p-2'>Director del CIAAC:</p></div>
+        <div style='padding-top: 3px; text-align: center;'>
+        <div class='row'>
+            <div class='column left'>
+            </div>
+            <div class='column middle'>
+                <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
+                    class='p-2'>Secretaria de
+                    Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
+                    Adiestramiento de
+                    Aviación Civil / SCT-AFAC-CIAAC</span><br>
+                <span style='font-weight: bold;' class='p-2'>Benjamín Romero Fuentes</span><br>
+                <span style='font-weight: bold; line-height:22px;' class='p-2'>Gral. de División P.A. DEMA en
+                    Ret.</span>
+            </div>
+            <div class='column right'>
+            <img style='float: right; width: 35%;' src='{$filename}'/>
+            </div>
+        </div>
+    </div>
+    <div>
+    <p class='p-2'>La presente <span style='font-weight: bold;'><u>Constancia</u></span> ampara los Temas vistos en el CURSO:
+    <span style='font-weight: bold;'>{$con['gstTitlo']}</span>, que a
+        continuación se enlistan:</p>
+    </div>
+    <div class='footer-constancia-gold'>
+    <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
+    class='p-2'>Secretaria de
+    Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
+    Adiestramiento de
+    Aviación Civil / SCT-AFAC-CIAAC</span>
+    </div>
+    <div class='footer-constancia'>
+    <span class='p-2'><span style='font-weight:bold;'>Lic. Rebeca Morales Reyes</span><br>Subdirectora de Diseño Pedagógico de Programas Aeronáuticos</span>
+</div>";
+} else if($con['gstCntnc'] == 'CERTIFICADO'){
+    echo "<div style='text-align: center;'>
+    <p class='CIAAC'>El Centro Internacional de Adiestramiento de Aviación Civil</p>
+    <p class='otorga'>Otorga el presente</p>
+    <p class='titulo-certificado'>{$con['gstCntnc']}</p>
+    <p class='nombre-persona'>Al C:. <span
+    class='nombre-persona-c'>{$nombresCompletos}</span></p>
+    <p class='otorga'>Por haber participado en el curso:</p>
+            <p class='nombre-curso'>{$con['gstTitlo']}</p> 
+    </p><span class='p-2'>Comprendido durante el periódo del {$con['dia']} de {$con['mesnombre']}
+    al {$con['diafinal']} de {$con['mesfinales']} del presente año, en la modalidad <span class='p-2' style='font-weight:bold;'>{$con['modalidad']}</span> impartido por el <span
+        class='p-2' style='font-weight:bold;'>{$con['sede']}</span> con una duración de {$con['gstDrcin']}<br><span style='padding-top: 80px;' class='p-2'>Ciudad de México, a
+        {$hoy}</span>
+        <p class='p-2'>Director del CIAAC:</p></div>
+        <div style='padding-top: 3px; text-align: center;'>
+        <div class='row'>
+            <div class='column left'>
+            </div>
+            <div class='column middle'>
+                <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
+                    class='p-2'>Secretaria de
+                    Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
+                    Adiestramiento de
+                    Aviación Civil / SCT-AFAC-CIAAC</span><br>
+                <span style='font-weight: bold;' class='p-2'>Benjamín Romero Fuentes</span><br>
+                <span style='font-weight: bold; line-height:22px;' class='p-2'>Gral. de División P.A. DEMA en
+                    Ret.</span>
+            </div>
+            <div class='column right'>
+            <img style='float: right; width: 35%;' src='{$filename}'/>
+            </div>
+        </div>
+    </div>
+    <div>
+    <p class='p-2'>Este certificado ampara los temas visto en el <span style='font-weight: bold;'>CURSO:
+           {$con['gstTitlo']}</span>, que a
+        continuación se enlistan:</p>
+    </div>
+    <div class='footer-constancia-gold'>
+    <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
+    class='p-2'>Secretaria de
+    Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
+    Adiestramiento de
+    Aviación Civil / SCT-AFAC-CIAAC</span>
+    </div>
+    <div class='footer-constancia'>
+    <span class='p-2'><span style='font-weight:bold;'>Lic. Rebeca Morales Reyes</span><br>Subdirectora de Diseño Pedagógico de Programas Aeronáuticos</span>
+</div>";
+}else {
+    echo "<div style='text-align: center;'>
+    <p class='CIAAC'>El Centro Internacional de Adiestramiento de Aviación Civil</p><br><br>
+    <p class='otorga'>Otorga el presente</p><br><br>
+    <p style='padding-top: 20px;' class='titulo-certificador'>RECONOCOMIENTO</p><br><br><br><br>
+    <p class='nombre-persona'>Al C:. <span
+    class='nombre-persona-c'>{$nombresCompletos}</span></p><br><br><br>
+    <p class='otorga'>Por haber participado en el curso:</p><br><br><br>
+    <p class='nombre-cursor'>{$con['gstTitlo']}</p><br><br><br>
+    </p><span class='p-3'>Impartido del {$con['dia']} de {$con['mesnombre']}
+    al {$con['diafinal']} de {$con['mesfinales']} del presente año</span>
+    <span class='p-3'>por la Escuela Militar de Graduados de Sanidad con lo establecido en el Convenio celebrado entre la Agencia Federal de Aviación Civil (AFAC), Dirección General de Protección y Medicina Preventiva (DGPyMPT) y la Secretaría de la Defensa Nacional (SEDENA).
+    <br><span style='padding-top: 80px;' class='p-2'>Ciudad de México, a
+    {$hoy}</span>
+    <p class='p-2'>Director del CIAAC:</p></div>
+    <div style='padding-top: 80%; text-align: center;'>
+    <div class='row'>
+        <div class='column left'>
+        </div>
+        <div class='column middle'>
+            <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
+                class='p-2'>Secretaria de
+                Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
+                Adiestramiento de
+                Aviación Civil / SCT-AFAC-CIAAC</span><br>
+            <span style='font-weight: bold;' class='p-2'>Benjamín Romero Fuentes</span><br>
+            <span style='font-weight: bold; line-height:22px;' class='p-2'>Gral. de División P.A. DEMA en
+                Ret.</span>
+        </div>
+        <div class='column right'>
+        <img style='float: right; width: 35%;' src='{$filename}'/>
+        </div>
+    </div>
+</div>
+<div>
+</div>
+<div style='padding-top: 200%' class='footer2'>
+<span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
+class='p-2'>Secretaria de
+Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
+Adiestramiento de
+Aviación Civil / SCT-AFAC-CIAAC</span>
+</div>
+</div>
 
 
-<table width="500px" cellpadding="5px" cellspacing="5px" border="1">
-    <tr bgcolor="#00FF00">
-        <td>Nombre</td>
-        <td>Apellido</td>
-        <td>Email</td>
-        <td>Carrera</td>
-        <td>Especialidad</td>
-    </tr>
-    <tr >
-        <td>angel Luis</td>
-        <td>albinagorta </td>
-        <td>albinagorta1998@gmail.com</td>
-        <td>Ing. Sistemas</td>
-        <td>Desarrollor de Software</td>
-    </tr>
-    <tr >
-        <td>Alcides</td>
-        <td>Camana</td>
-        <td>Camana@gmail.com</td>
-        <td>Ing. Sistemas</td>
-        <td>Analista de Software</td>
-    </tr>
-    <tr>
-        <td>judiht</td>
-        <td>Mayta</td>
-        <td>mayta@gmail.com</td>
-        <td>Ing. Sistemas</td>
-        <td>Desarrollor de Software</td>
-    </tr>
-    <tr>
-        <td>Milagros</td>
-        <td>Almerco</td>
-        <td>almerca@gmail.com</td>
-        <td>Ing. Sistemas</td>
-        <td>Desarrollor de Software</td>
-    </tr>       
-    <tr>
-        <td>Victor</td>
-        <td>Zurita</td>
-        <td>zuritav@gmail.com</td>
-        <td>Negocios Internacionales</td>
-        <td>Negocios Internacionales</td>
-    </tr>
-  
-</table>
+
+    ";
+}
+?>
+
+
+    
+</body>
+</html>
+
+
+
+
+
+
+
 <?php
 require_once '../dist/dompdf/autoload.inc.php';
 
 use Dompdf\Dompdf;
 $dompdf = new DOMPDF();
+$dompdf->set_paper('A4', 'landscape');
 $dompdf->load_html(ob_get_clean());
 $dompdf->render();
+$dompdf->stream("", array("Attachment" => 0));
 $pdf = $dompdf->output();
 $filename = "constancias/constancias-CIAAC.pdf";
 file_put_contents($filename, $pdf);
