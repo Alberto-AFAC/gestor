616975b11918a08e67b6613b5237e18dcaa10a31
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-09-28 15:55:19 -0500

    Ya estan las constancias

--- admin/constancia.php
+++ admin/constancia.php
@@ -308,93 +308,44 @@ if($con['gstCntnc'] == 'CONSTANCIA'){
 </div>";
 }else {
     echo "<div style='text-align: center;'>
-    <p class='CIAAC'>El Centro Internacional de Adiestramiento de Aviación Civil</p>
-    <p class='otorga'>Otorga el presente</p>
-    <p class='titulo-certificador'>RECONOCOMIENTO</p>
+    <p class='CIAAC'>El Centro Internacional de Adiestramiento de Aviación Civil</p><br><br>
+    <p class='otorga'>Otorga el presente</p><br><br>
+    <p style='padding-top: 20px;' class='titulo-certificador'>RECONOCOMIENTO</p><br><br><br><br>
     <p class='nombre-persona'>Al C:. <span
-    class='nombre-persona-c'>{$nombresCompletos}</span></p>
-    <p class='otorga'>Por haber participado en el curso:</p>
-     <p class='nombre-cursor'>{$con['gstTitlo']}</p> 
+    class='nombre-persona-c'>{$nombresCompletos}</span></p><br><br><br>
+    <p class='otorga'>Por haber participado en el curso:</p><br><br><br>
+    <p class='nombre-cursor'>{$con['gstTitlo']}</p><br><br><br>
     </p><span class='p-3'>Impartido del {$con['dia']} de {$con['mesnombre']}
     al {$con['diafinal']} de {$con['mesfinales']} del presente año</span>
     <span class='p-3'>por la Escuela Militar de Graduados de Sanidad con lo establecido en el Convenio celebrado entre la Agencia Federal de Aviación Civil (AFAC), Dirección General de Protección y Medicina Preventiva (DGPyMPT) y la Secretaría de la Defensa Nacional (SEDENA).
     <br><span style='padding-top: 80px;' class='p-2'>Ciudad de México, a
-        {$hoy}</span>
-        <p class='p-2'>Director del CIAAC:</p></div>
-        <div style='padding-top: 5px; text-align: center;'>
-        <div class='row'>
-            <div class='column left'>
-            </div>
-            <div class='column middle'>
-                <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
-                    class='p-2'>Secretaria de
-                    Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
-                    Adiestramiento de
-                    Aviación Civil / SCT-AFAC-CIAAC</span><br>
-                <span style='font-weight: bold;' class='p-2'>Benjamín Romero Fuentes</span><br>
-                <span style='font-weight: bold; line-height:22px;' class='p-2'>Gral. de División P.A. DEMA en
-                    Ret.</span>
-            </div>
-            <div class='column right'>
+    {$hoy}</span>
+    <p class='p-2'>Director del CIAAC:</p></div>
+    <div style='padding-top: 80%; text-align: center;'>
+    <div class='row'>
+        <div class='column left'>
+        </div>
+        <div class='column middle'>
+            <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
+                class='p-2'>Secretaria de
+                Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
+                Adiestramiento de
+                Aviación Civil / SCT-AFAC-CIAAC</span><br>
+            <span style='font-weight: bold;' class='p-2'>Benjamín Romero Fuentes</span><br>
+            <span style='font-weight: bold; line-height:22px;' class='p-2'>Gral. de División P.A. DEMA en
+                Ret.</span>
+        </div>
+        <div class='column right'>
             <img style='float: right; width: 35%;' src='{$filename}'/>
             </div>
         </div>
     </div>
     <div>
     </div><br>
-    <div class='footer2'>
-    <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
-    class='p-2'>Secretaria de
-    Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
-    Adiestramiento de
-    Aviación Civil / SCT-AFAC-CIAAC</span>
-    </div>
-</div>";
+    ";
 }
 ?>
-    <!-- <div style="text-align: center;">
-        <p class="CIAAC">El Centro Internacional de Adiestramiento de Aviación Civil</p>
-        <p class="otorga">Otorga el presente</p>
-        <p class="titulo-certificado"><?php echo $con['gstCntnc']?></p>
-        <p class="nombre-persona">Al C:. <span
-                class="nombre-persona-c"><?php echo $con['gstNombr']." ".$con['gstApell'] ?></span></p>
-        <p class="otorga">Por haber participado en el curso:</p>
-        <p class="nombre-curso"><?php echo $con['gstTitlo']?></p>
-        <span class="p-2">Comprendido durante el periódo del <?php echo $con['dia']?> de <?php echo $con['mesnombre']?>
-            al <?php echo $con['diafinal']?> de <?php echo $con['mesfinales']?> del presente año, en la modalidad
-            <span class="p-2" style="font-weight:bold;"><?php echo $con['modalidad']?></span> impartido por el <span
-                class="p-2" style="font-weight:bold;"><?php echo $con['sede']?></span> con una duración de
-            <?php echo $con['gstDrcin']?><br><span style="padding-top: 80px;" class="p-2">Ciudad de México, a
-                <?php echo $hoy?></span>
-            <p class="p-2">Director del CIAAC:</p>
-    </div> -->
-    <!-- <div style="padding-top: 5px; text-align: center;">
-        <div class="row">
-            <div class="column left">
-            </div>
-            <div class="column middle">
-                <span style="padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;"
-                    class="p-2">Secretaria de
-                    Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
-                    Adiestramiento de
-                    Aviación Civil / SCT-AFAC-CIAAC</span><br>
-                <span style="font-weight: bold;" class="p-2">Benjamín Romero Fuentes</span><br>
-                <span style="font-weight: bold; line-height:22px;" class="p-2">Gral. de División P.A. DEMA en
-                    Ret.</span>
-            </div>
-            <div class="column right">
-                <?php 
-    echo '<img style="float: right; width: 35%;" src="'.$filename.'" />';
-        ?>
-            </div>
-        </div>
-    </div>
-    
-    <div>
-        <p class="p-2">Este certificado ampara los temas visto en el <span style="font-weight: bold;">CURSO:
-                <?php echo $con['gstTitlo']?></span>, que a
-            continuación se enlistan:</p>
-        </div> -->
+    <div style='page-break-after:always;'></div>
         <div>
         <?php 
        $datos = $_GET['data'];
@@ -417,18 +368,19 @@ if($con['gstCntnc'] == 'CONSTANCIA'){
         <?php } ?>
 
     </div>
-    <!-- <?php 
-        $datos = $_GET['data'];
-        $queryTemario1 = "SELECT idtem, titulo,idcurso, libro, numero, afojas FROM temario WHERE idcurso = $idc";
-        $const1 = mysqli_query($conexion, $queryTemario1);
-        $consulta2 = mysqli_fetch_array($const1)
-
-        ?> -->
+   
     <div class="afojas1">
         <span>Registrado bajo el No.<?php echo $con['gstIdlsc'];?></span>
-        <!-- a fojas
-            <?php echo $consulta2['afojas'];?> del libro de <?php echo $consulta2['libro'];?> -->
     </div>
+    
+    <div class='footer2'>
+    <span style='padding-top: 120px; font-size: 8px; font-weight: bold; color: #996633;'
+    class='p-2'>Secretaria de
+    Comunicaciones y Transportes - Agencia Federal de Aviación Civil– Centro Internacional de
+    Adiestramiento de
+    Aviación Civil / SCT-AFAC-CIAAC</span>
+    </div>
+</div>
    
     <?php
         // require_once 'dompdf/autoload.inc.php';
