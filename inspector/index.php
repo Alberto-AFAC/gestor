23de34a0237f83364365f524c729317e6a85a262
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-10-12 10:19:35 -0500

    ok

--- inspector/index.php
+++ inspector/index.php
@@ -68,7 +68,7 @@
                             <div class="content">
                                 <h3>MESA DE AYUDA</h3><br>
                                 <img class="img-servicios" src="../models/menu/images/ingenieria.svg" width="75%;" alt="tecnico"><br><br>
-                                <a href="#">Iniciar</a>
+                                <a href="https://mesa-ayuda.afac-avciv.com/">Iniciar</a>
                             </div>
                         </div>
                         <div class="card">
@@ -82,7 +82,7 @@
                     </div>
                 </div>
             </li>
-          <li  data-depth="0.40">
+          <!-- <li  data-depth="0.40">
                 <div class="depth-1 flake1">
                     <img style="z-index: -9999;" alt="flake" src="../models/menu/images/flakes/depth1/flakes1.png">
                 </div>
@@ -98,17 +98,17 @@
                  <div class="depth-1 flake4">
                     <img alt="flake" src="../models/menu/images/flakes/depth1/flakes4.png">
                 </div>
-            </li> 
+            </li>  -->
 
-            <li class="" data-depth="0.50">
+            <!-- <li class="" data-depth="0.50"> -->
                 <!-- <div class="depth-2 flake1">
                     <img alt="flake" src="images/flakes/depth2/flakes1.png">
                 </div> -->
 
-                <div class="depth-2 flake2">
+                <!-- <div class="depth-2 flake2">
                     <img alt="flake" src="../models/menu/images/flakes/depth2/flakes2.png">
                 </div>
-            </li>
+            </li> -->
  <!--  
             <li class="layer" data-depth="0.60">
                 <div class="depth-3 flake1">
@@ -134,11 +134,11 @@
                 </div>
             </li>  -->
 
-            <li class="" data-depth="1.00">
+            <!-- <li class="" data-depth="1.00">
                 <div class="depth-5">
                     <img alt="flake" src="../models/menu/images/flakes/depth5/flakes.png">
                 </div>
-            </li>
+            </li> -->
         </ul>
     </div>
     <script src="../js/menu/js/jquery.js"></script>
