093232dc7a576a10d71efbc62c2fa60da3cfaf51
angelcansecoc <angelcanseco.c@gmail.com>
2021-10-25 18:03:52 -0500

    ok

--- js/datos.js
+++ js/datos.js
@@ -1495,7 +1495,9 @@ function inspector(gstIdper) {
                 } else {
                     $("#ocultar1").show();
                     $("#ocultar2").show();
+
                     document.getElementById('evaluaciones').innerHTML = '<img src="../dist/img/check.svg" alt="YES" width="25px;">';
+
                 }
 
 
