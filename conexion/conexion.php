106c9aa03ad90e12501f48a9ad73380acd5aeb61
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-11-04 15:02:34 -0600

    ok

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,8 +1,8 @@
 <?php 
 	
 	//instancia clase mysqli para la conexion al servidor base de datos- 4 parametros 
-	// $conexion = new mysqli('localhost','u683645102_root','Agencia.SCT2021.','u683645102_gestor');
-	        $conexion = new mysqli('localhost','root','','gestor');
+	$conexion = new mysqli('localhost','u683645102_root','Agencia.SCT2021.','u683645102_gestor');
+	        // $conexion = new mysqli('localhost','root','','gestor');
 	//si mustra un errro al momento de querer conectarse 
 	if ($conexion->connect_error):
 			echo "Error de Conexión".$conexion->connect_error;
