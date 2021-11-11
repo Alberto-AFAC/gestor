124bde2ef12177d3c7373d936a6170727acbd71c
angelcansecoc <angelcanseco.c@gmail.com>
2021-11-11 14:25:54 -0600

    ultima actualizacion

--- php/data-inspeccion.php
+++ php/data-inspeccion.php
@@ -4,7 +4,7 @@
 	session_start();
 
 
-
+$n=0;
 	$query = "SELECT *, DATE_FORMAT(personal.gstFeing, '%d/%m/%Y') as Ingreso,personal.gstIDCat AS IDcat  FROM personal 
     INNER JOIN categorias ON categorias.gstIdcat = personal.gstIDCat
     WHERE personal.gstCargo = 'INSPECTOR' AND  personal.estado = 0 OR personal.gstCargo = 'DIRECTOR' AND  personal.estado = 0 ORDER BY gstIdper DESC";
@@ -13,8 +13,9 @@
 	if(!$resultado){
 		die("error");
 	}else{
+		
 		while($data = mysqli_fetch_assoc($resultado)){
-	
+			
             $fechaActual = date_create(date('d-m-Y')); 
 		    $FechaIngreso = date_create($data['gstFeing']); 
 		    $interval = date_diff($FechaIngreso, $fechaActual,false);  
@@ -29,6 +30,7 @@
 	  FROM cursos 
 	  WHERE idinsp  = $gstIdper AND proceso = 'FINALIZADO'";
 	  $resultado2 = mysqli_query($conexion, $query2);
+	  
 	  if($curs = mysqli_fetch_row($resultado2)){ 
   
 	  $cursor = "<span style='font-weight: bold; height: 50px; color: #3C8DBC;'>Personal antiguo</span>";
@@ -36,7 +38,7 @@
 	  }else{
 	  $cursor = "<span style='font-weight: bold; height: 50px; color: green;'>Nuevo ingreso</span>";
 	  }
-
+	  $n++;
     //   if($antiguedad <=30){
     //     $antiguedadT = '<span style="font-weight: bold; height: 50px; color: green;">Nuevo ingreso</span>';
     // }else {
@@ -51,11 +53,12 @@
         $total =  "<a type='button' title='Por evaluación' onclick='inspector({$gstIdper})' class='btn btn-warning'  data-toggle='modal' data-target='#modal-evaluar' ><i class='fa ion-android-clipboard' style='font-size:23px;'></i></a> <a href='javascript:openDtlls()' title='Perfil' onclick='inspector({$gstIdper})' class='datos btn btn-default'><i class='glyphicon glyphicon-user text-success'></i></a> <a type='button' title='Agregar estudios' onclick='estudio({$gstIdper})' class='btn btn-default' data-toggle='modal' data-target='#modal-estudio'><i class='fa fa-graduation-cap text-info'></i></a> <a type='button' title='Agregar experiencia profesional' onclick='profesion({$gstIdper})' class='btn btn-default' data-toggle='modal' data-target='#modal-profesion'><i class='fa fa-suitcase text-info'></i></a>";
 
             }else if($data['gstEvalu'] == 'SI') {
+
         $total = "<a type='button' title='Evaluado' onclick='resultado({$resul})' class='datos btn btn-success'  data-toggle='modal' data-target='#modal-resultado'><i class='fa ion-android-clipboard' style='font-size:23px;'></i></a> <a href='javascript:openDtlls()' title='Perfil' onclick='inspector({$gstIdper})' class='datos btn btn-default'><i class='glyphicon glyphicon-user text-success'></i></a> <a type='button' title='Agregar estudios' onclick='estudio({$gstIdper})' class='btn btn-default' data-toggle='modal' data-target='#modal-estudio'><i class='fa fa-graduation-cap text-info'></i></a> <a type='button' title='Agregar experiencia profesional' onclick='profesion({$gstIdper})' class='btn btn-default' data-toggle='modal' data-target='#modal-profesion'><i class='fa fa-suitcase text-info'></i></a>";
 
             }
  	
-	 $caledario[] = [ $data["gstNmpld"],$data["gstNombr"],$data["gstApell"],$data["gstCatgr"],$data["Ingreso"], $cursor, $total];
+	 $caledario[] = [ $n,$data["gstNmpld"],$data["gstNombr"],$data["gstApell"],$data["gstCatgr"],$data["Ingreso"], $cursor, $total];
 
 		}
 	}
