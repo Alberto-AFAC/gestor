a55ae3b6eda322c840b274d2435f39e791de1e03
angelcansecoc <angelcanseco.c@gmail.com>
2021-11-29 17:49:40 -0600

    ok

--- php/insertProfile.php
+++ php/insertProfile.php
@@ -4,18 +4,19 @@ include ('../conexion/conexion.php');
 $opciones = $_POST['opciones'];
 $informacion = [];
 
-if($opciones === 'agregar'){
-
 $date=$_POST['date'];
 $base64=$_POST['base64'];
 $id_persona=$_POST['id_persona'];
+
+if($opciones === 'agregar'){
+
 $sql="INSERT INTO profile (id_persona,base64,date)
         values ('$id_persona','$base64','$date')";
 echo mysqli_query($conexion,$sql);
 
 }else if($opciones === 'editar'){
 
-        $sql="UPDATE profile SET id_persona='$id_persona', base64='$base64',date='$date' WHERE id_persona = $id_persona";
+        $sql="UPDATE profile SET base64='$base64',date='$date' WHERE id_persona = $id_persona";
 echo mysqli_query($conexion,$sql);
 
 
