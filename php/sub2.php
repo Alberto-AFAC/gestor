4b88f897ef68243219ecf8b870fc9a336b3833b4
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-12-09 09:57:15 -0600

    sub2

---
+++ php/sub2.php
@@ -0,0 +1,27 @@
+<?php
+	include("../conexion/conexion.php");
+	session_start();
+	$query = "SELECT id_subojt, idtarea, ojt_subtarea, numsubt, ojts_subs.estado from ojts_subs
+	INNER JOIN ojts ON ojts.id_ojt = ojts_subs.idtarea 
+	WHERE ojts_subs.estado = 0 AND numsubt = 2";
+	$resultado = mysqli_query($conexion, $query);
+
+	if(!$resultado){
+		die("error");
+	}else{
+		while($data = mysqli_fetch_assoc($resultado)){
+
+			$arreglo["data"][] = $data; 
+		}
+		if(isset($arreglo)&&!empty($arreglo)){
+
+			echo json_encode($arreglo);
+		}else{
+
+			echo $arreglo='0';
+		}
+	}
+		mysqli_free_result($resultado);
+		mysqli_close($conexion);
+
+?>
