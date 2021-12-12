22c0ac8473029967dce4272eff4e8b243275aae8
angelcansecoc <angelcanseco.c@gmail.com>
2021-12-12 14:43:00 -0600

    123

--- php/curConfir.php
+++ php/curConfir.php
@@ -11,26 +11,33 @@
 	if(!$resultado){
 		die("error");
 	}else{
-		
+
 		while($data = mysqli_fetch_assoc($resultado)){
 
 		$idinsp = $data['idinsp'];
 
+
 			if($data['idcoor']==$data['idinsp']){
-				$data["puesto"] = 'COORDINADOR';
+			$data["puesto"] = 'COORDINADOR';
 			}else{
-				$data["puesto"] = 'INSPECTOR';
+			$data["puesto"] = 'INSPECTOR';
 			}
+		$idper = '';
+
 
 			$queriy = "SELECT * FROM instructor WHERE id_per = $idinsp AND estado = 0";
 			$result = mysqli_query($conexion, $queriy);
 
-			if($res = mysqli_fetch_array($result)){
+			if($rest = mysqli_fetch_array($result)){
 				$data["puesto"] = 'INSTRUCTOR';
+				$idper = $rest['id_per'];
 			}			
-			if($data['idcoor']==$data['idinsp'] && $respon = 'INSTRUCTOR'){
+
+
+			if($data['idcoor']==$data['idinsp'] && $data['idcoor'] == $idper){
 				$data["puesto"] = 'INSTCOOR';
 			}
+				
 
 
 			$arreglo["data"][] = $data; 
