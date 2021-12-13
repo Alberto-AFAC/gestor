bcebfeee106a2fba1978e32a2b14742f0aa51386
angelcansecoc <angelcanseco.c@gmail.com>
2021-12-13 17:20:36 -0600

    OK

--- admin/header.php
+++ admin/header.php
@@ -109,7 +109,7 @@ ini_set('date.timezone','America/Mexico_City');
                     <li><a href="pronostico"><i class="fa fa-hourglass-half"></i> Pronostico de Cursos</a></li>
                     <!-- <li><a href="estadisticas.php"><i class="fa fa-pie-chart"></i> Estadisticas Generales</a></li> -->
                     <li><a href="niveldesatis"><i class="fa fa-line-chart"></i>Nivel de satisfacción</a></li>
-                    <li><a title="Historial de Constancias, Certificados y Diplomas" href="constancias"><i class="fa fa-certificate"></i>Historial de Constancias...</a></li>
+                    <li><a title="Historial de Constancias, Certificados y Diplomas" href="constancias"><i class="fa fa-certificate"></i>Historial Constancias...</a></li>
                 </ul>
             </li>
             <!--  -->
