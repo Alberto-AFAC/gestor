c492e45c681673548da0ee53c5e313b18e2bb46e
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-12-21 11:40:47 -0600

    enviarMail

--- admin/enviarMailResp.php
+++ admin/enviarMailResp.php
@@ -42,7 +42,7 @@ $mail->msgHTML(file_get_contents('message.html'), __DIR__);
 $mail->isHTML(true);                                  //Set email format to HTML
 $mail->Subject = 'NUEVO CURSO PROGRAMADO';
 $mail->CharSet = 'UTF-8';
-$body = '<table style="width: 100%; border: 1px solid black";><tr><th>No.</th><th>PARTICIPANTES DEL CURSO</th></tr>';
+$body = '<table style="border-collapse: collapse; width: 100%; border: 1px solid black";><tr><th style="border-collapse: collapse; border: 1px solid black";>No.</th><th style="border-collapse: collapse; border: 1px solid black";>PARTICIPANTES DEL CURSO</th></tr>';
         while($curso = mysqli_fetch_assoc($resultado)){
             $x++;
             $to_array = explode(',', $correoRs);
