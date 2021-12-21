ba5b49f0ba497951e26df6094e27d297a153f0a2
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-12-21 10:49:46 -0600

    enviarMAil

--- admin/enviarMail.php
+++ admin/enviarMail.php
@@ -21,16 +21,23 @@ require '../php-mailer2/SMTP.php';
 		} else{
 			$to = $curso['gstCinst'];
 		}
-
-		$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->SMTPDebug = 2;
-$mail->Host = 'smtp.hostinger.com';
-$mail->SMTPSecure = 'ssl';                          
-$mail->Port = 465;
-$mail->SMTPAuth = true;
-$mail->Username = 'notificaciones@afac-avciv.com';
-$mail->Password = 'Agencia.SCT.2021.';
+		$mail = new PHPMailer();
+		$mail->isSMTP();
+		$mail->Host = 'smtp.mailtrap.io';
+		$mail->SMTPAuth = true;
+		$mail->Port = 2525;
+		$mail->Username = '10a376e8596ee9';
+		$mail->Password = 'c2aeed30f4cf96';
+		$mail->SMTPSecure = 'tls';  
+// 		$mail = new PHPMailer;
+// $mail->isSMTP();
+// $mail->SMTPDebug = 2;
+// $mail->Host = 'smtp.hostinger.com';
+// $mail->SMTPSecure = 'ssl';                          
+// $mail->Port = 465;
+// $mail->SMTPAuth = true;
+// $mail->Username = 'notificaciones@afac-avciv.com';
+// $mail->Password = 'Agencia.SCT.2021.';
 $mail->setFrom('notificaciones@afac-avciv.com', 'NOTIFICACIONES AFAC');
 $mail->addAddress("jmondragonescamilla@gmail.com");
 $mail->Subject = 'NUEVO CURSO PROGRAMADO';
