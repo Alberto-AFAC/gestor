97d4e45fad7f113d71b11621d90c278ea27abebb
Alberto-AFAC <jmondragonescamilla@gmail.com>
2022-01-04 13:24:19 -0600

    Cursos para personal externo

--- php/insPerExt.php
+++ php/insPerExt.php
@@ -9,9 +9,9 @@
     $gstCasa=$_POST['gstCasa'];
     $gstClulr=$_POST['gstClulr'];
     $gstCorro=$_POST['gstCorro'];
-    // $gstSpcID=$_POST['gstSpcID'];
+    $gstSpcID=$_POST['gstSpcID'];
 
-    $sql = "INSERT INTO personal VALUES(0,'$gstNombr','$gstApell','0','0','0','0','$gstCurp','$gstRfc','0','0','0','0','0','0','0','0','0','0','0','0','$gstCasa','$gstClulr','0','0','0','0','0','0','0','0','0','0','0','$gstIDCat','0','$gstCorro','0','0','0','0','0','0','NO',0,'0',2)";
+    $sql = "INSERT INTO personal VALUES(0,'$gstNombr','$gstApell','0','0','0','0','$gstCurp','$gstRfc','0','0','0','0','0','0','0','0','0','0','0','0','$gstCasa','$gstClulr','0','0','0','0','0','0','0','0','$gstSpcID','0','INSTRUCTOR','$gstIDCat','0','$gstCorro','0','0','0','0','0','0','NO',0,'0',2)";
 
 
     echo mysqli_query($conexion,$sql);
