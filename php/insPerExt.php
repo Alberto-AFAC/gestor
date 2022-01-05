6e335ec6ffc0ed8025578d5deea6e5970a83b0ea
Alberto-AFAC <jmondragonescamilla@gmail.com>
2022-01-05 10:45:22 -0600

    SE QUITA LA ESPECIALIDAD

--- php/insPerExt.php
+++ php/insPerExt.php
@@ -5,13 +5,13 @@
     $gstCurp=$_POST['gstCurp'];
     $gstRfc=$_POST['gstRfc'];
     $gstSexo=$_POST['gstSexo'];
-    $gstIDCat=$_POST['gstIDCat'];
+    // $gstIDCat=$_POST['gstIDCat'];
     $gstCasa=$_POST['gstCasa'];
     $gstClulr=$_POST['gstClulr'];
     $gstCorro=$_POST['gstCorro'];
     $gstSpcID=$_POST['gstSpcID'];
 
-    $sql = "INSERT INTO personal VALUES(0,'$gstNombr','$gstApell','0','0','0','0','$gstCurp','$gstRfc','0','0','0','0','0','0','0','0','0','0','0','0','$gstCasa','$gstClulr','0','0','0','0','0','0','0','0','$gstSpcID','0','INSTRUCTOR','$gstIDCat','0','$gstCorro','0','0','0','0','0','0','NO',0,'0',2)";
+    $sql = "INSERT INTO personal VALUES(0,'$gstNombr','$gstApell','0','0','0','0','$gstCurp','$gstRfc','0','0','0','0','0','0','0','0','0','0','0','0','$gstCasa','$gstClulr','0','0','0','0','0','0','0','0','$gstSpcID','0','INSTRUCTOR','0','0','$gstCorro','0','0','0','0','0','0','NO',0,'0',2)";
 
 
     echo mysqli_query($conexion,$sql);
