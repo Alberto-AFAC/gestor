e549b163d51c5bf22b7021fdd95370057b7782bd
Alberto-AFAC <jmondragonescamilla@gmail.com>
2022-01-05 10:34:59 -0600

    INSPECTOR PARA LISTADO DE PERSONAL EXTERNO

--- php/insertarPersonal.php
+++ php/insertarPersonal.php
@@ -11,7 +11,7 @@
     $gstCorro=$_POST['gstCorro'];
     $gstSpcID=$_POST['gstSpcID'];
 
-    $sql = "INSERT INTO personal VALUES(0,'$gstNombr','$gstApell','0','0','0','0','$gstCurp','$gstRfc','0','0','0','0','0','0','0','0','0','0','0','0','$gstCasa','$gstClulr','0','0','0','0','0','0','0','0','$gstSpcID','0','INSTRUCTOR','$gstIDCat','0','$gstCorro','0','0','0','0','0','0','NO',0,'0',3)";
+    $sql = "INSERT INTO personal VALUES(0,'$gstNombr','$gstApell','0','0','0','0','$gstCurp','$gstRfc','0','0','0','0','0','0','0','0','0','0','0','0','$gstCasa','$gstClulr','0','0','0','0','0','0','0','0','$gstSpcID','0','INSPECTOR','$gstIDCat','0','$gstCorro','0','0','0','0','0','0','NO',0,'0',3)";
 
 
     echo mysqli_query($conexion,$sql);
