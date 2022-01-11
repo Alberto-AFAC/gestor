1ee9be9a0aef118393b9fc88a5007db6ab9ca463
Alberto-AFAC <jmondragonescamilla@gmail.com>
2022-01-11 15:35:38 -0600

    mail

--- admin/enviarMailResp.php
+++ admin/enviarMailResp.php
@@ -46,6 +46,8 @@ $mail->CharSet = 'UTF-8';
 $body = '<p>NOMBRE DEL CURSO: <span style="font-weight: bold;">'.$curso['gstTitlo'].'</span></p>
 <p>FECHA DE IMPARTICIÓN: <span style="font-weight: bold;">'.$curso['inicia'].'</span></p>
 <p>MODALIDAD: <span style="font-weight: bold;">'.$curso['modalidad'].'</span></p>
+<p>COORDINADOR: <span style="font-weight: bold;"></span></p>
+<p>INSTRUCTOR: <span style="font-weight: bold;"></span></p>
 EL CURSO ESTÁ DIRIGIDO AL PERSONAL QUE A CONTINUACIÓN SE ENLISTA:<br><br>
 <table style="border-collapse: collapse; width: 100%; border: 1px solid black";><tr><th style="border-collapse: collapse; border: 1px solid black";>No.</th><th style="border-collapse: collapse; border: 1px solid black";>PARTICIPANTES DEL CURSO</th></tr>';
         while($curso = mysqli_fetch_assoc($resultado)){
