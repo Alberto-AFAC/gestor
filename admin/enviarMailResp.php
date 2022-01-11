a51e135f0d98f2b29c483d397a53c7e6c921999e
Alberto-AFAC <jmondragonescamilla@gmail.com>
2022-01-11 16:25:24 -0600

    responsables

--- admin/enviarMailResp.php
+++ admin/enviarMailResp.php
@@ -17,6 +17,12 @@ require '../php-mailer2/SMTP.php';
 	$resultado = mysqli_query($conexion, $query);
     $curso = mysqli_fetch_assoc($resultado);
     $x = 0;
+    if($curso['gstCargo'] == 'INSTRUCTOR'){
+        $instructor = $curso['gstNombr'].' '.$curso['gstApell'];
+
+    }else if($curso['gstCargo'] == 'COORDINADOR'){
+        $coordinador = $curso['gstNombr'].' '.$curso['gstApell'];
+    }
 		
 
 // 		$mail = new PHPMailer;
@@ -46,8 +52,8 @@ $mail->CharSet = 'UTF-8';
 $body = '<p>NOMBRE DEL CURSO: <span style="font-weight: bold;">'.$curso['gstTitlo'].'</span></p>
 <p>FECHA DE IMPARTICIÓN: <span style="font-weight: bold;">'.$curso['inicia'].'</span></p>
 <p>MODALIDAD: <span style="font-weight: bold;">'.$curso['modalidad'].'</span></p>
-<p>COORDINADOR: <span style="font-weight: bold;"></span></p>
-<p>INSTRUCTOR: <span style="font-weight: bold;"></span></p>
+<p>COORDINADOR: <span style="font-weight: bold;">'.$coordinador.'</span></p>
+<p>INSTRUCTOR: <span style="font-weight: bold;">'.$instructor.'</span></p>
 EL CURSO ESTÁ DIRIGIDO AL PERSONAL QUE A CONTINUACIÓN SE ENLISTA:<br><br>
 <table style="border-collapse: collapse; width: 100%; border: 1px solid black";><tr><th style="border-collapse: collapse; border: 1px solid black";>No.</th><th style="border-collapse: collapse; border: 1px solid black";>PARTICIPANTES DEL CURSO</th></tr>';
         while($curso = mysqli_fetch_assoc($resultado)){
