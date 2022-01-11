906e636f7ee1867310c6d8f3975fddc37ff492f0
Alberto-AFAC <jmondragonescamilla@gmail.com>
2022-01-11 16:43:31 -0600

    correo

--- admin/enviarMailResp.php
+++ admin/enviarMailResp.php
@@ -26,11 +26,9 @@ require '../php-mailer2/SMTP.php';
             INNER JOIN cursos ON idmstr = gstIdlsc
             INNER JOIN personal ON gstIdper = idinsp 
         WHERE
-            codigo = '$idcurso' AND gstCargo = 'COORDINADOR'";
+            codigo = '$idcurso' AND gstCargo = 'INSTRUCTOR'";
             	$resultado2 = mysqli_query($conexion, $query);
-                while($coordinador = mysqli_fetch_assoc($resultado2)){
-                    $coordinador = $coordinador['gstNombr'];
-                }
+                $coordinador = mysqli_fetch_assoc($resultado2);
     $x = 0;
   
 		
@@ -62,7 +60,7 @@ $mail->CharSet = 'UTF-8';
 $body = '<p>NOMBRE DEL CURSO: <span style="font-weight: bold;">'.$curso['gstTitlo'].'</span></p>
 <p>FECHA DE IMPARTICIÓN: <span style="font-weight: bold;">'.$curso['inicia'].'</span></p>
 <p>MODALIDAD: <span style="font-weight: bold;">'.$curso['modalidad'].'</span></p>
-<p>COORDINADOR: <span style="font-weight: bold;">--> '.$coordinador.'</span></p>
+<p>COORDINADOR: <span style="font-weight: bold;">--> '.$coordinador['gstNombr'].'</span></p>
 <p>INSTRUCTOR: <span style="font-weight: bold;">---> ES EL INSTRUCTOR'.$instructor.'</span></p>
 EL CURSO ESTÁ DIRIGIDO AL PERSONAL QUE A CONTINUACIÓN SE ENLISTA:<br><br>
 <table style="border-collapse: collapse; width: 100%; border: 1px solid black";><tr><th style="border-collapse: collapse; border: 1px solid black";>No.</th><th style="border-collapse: collapse; border: 1px solid black";>PARTICIPANTES DEL CURSO</th></tr>';
