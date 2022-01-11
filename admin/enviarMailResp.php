a93a95dd00ce0bdbbaf3b2b3743d4c29d25d2daa
Alberto-AFAC <jmondragonescamilla@gmail.com>
2022-01-11 17:08:25 -0600

    cargo en correo

--- admin/enviarMailResp.php
+++ admin/enviarMailResp.php
@@ -71,7 +71,7 @@ EL CURSO ESTÁ DIRIGIDO AL PERSONAL QUE A CONTINUACIÓN SE ENLISTA:<br><br>
             {
                 $mail->addAddress($address, 'Usuario');
             }
-            $body .= "<tr><td style='border-collapse: collapse; border: 1px solid black';>".$x.".-</td><td style='border-collapse: collapse; border: 1px solid black';>".$curso['gstNombr']." ".$curso['gstApell']." ".$curso['gstCargo']."</td></tr>"; 
+            $body .= "<tr><td style='border-collapse: collapse; border: 1px solid black';>".$x.".-</td><td style='border-collapse: collapse; border: 1px solid black';>".$curso['gstNombr']." ".$curso['gstApell']." ".($curso['gstCargo'])."</td></tr>"; 
 
 		// $msg .= "MENSAJE DE PRUEBA PARA CORREOS AL RESPONSABLE";
     }
