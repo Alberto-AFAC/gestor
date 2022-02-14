1b0dee6135b31ab30f3771b7afa9428a74dbea79
angelcansecoc <angelcanseco.c@gmail.com>
2022-02-14 17:07:04 -0600

    ok

--- js/datos.js
+++ js/datos.js
@@ -1624,18 +1624,23 @@ function adjuntarOjt() {
         contentType: false,
         processData: false,
         success: function(r) {
+
+            $('#nota').show();    
+
             if (r == 8) {
                 $('#vaciojt').toggle('toggle');
                 setTimeout(function() {
                     $('#vaciojt').toggle('toggle');
                 }, 4000);
 
+                $('#nota').hide();  
+
             } else if (r == 0) {
                 $('#exitojt').toggle('toggle');
                 setTimeout(function() {
                     $('#exitojt').toggle('toggle');
                 }, 4000);
-
+                $('#nota').hide();  
                 consultardocIns(ojtIdper);
 
             } else if (r == 1) {
@@ -1643,31 +1648,37 @@ function adjuntarOjt() {
                 setTimeout(function() {
                     $('#fallajt').toggle('toggle');
                 }, 4000);
+                $('#nota').hide();                  
             } else if (r == 2) {
                 $('#errorjt').toggle('toggle');
                 setTimeout(function() {
                     $('#errorjt').toggle('toggle');
                 }, 4000);
+                $('#nota').hide();                  
             } else if (r == 3) {
                 $('#renomjt').toggle('toggle');
                 setTimeout(function() {
                     $('#renomjt').toggle('toggle');
                 }, 4000);
+                $('#nota').hide();                  
             } else if (r == 4) {
                 $('#fornjt').toggle('toggle');
                 setTimeout(function() {
                     $('#fornjt').toggle('toggle');
                 }, 4000);
+                $('#nota').hide();                  
             } else if (r == 6) {
                 $('#adjuntajt').toggle('toggle');
                 setTimeout(function() {
                     $('#adjuntajt').toggle('toggle');
                 }, 4000);
+                $('#nota').hide();                  
             } else if (r == 7) {
                 $('#repetidojt').toggle('toggle');
                 setTimeout(function() {
                     $('#repetidojt').toggle('toggle');
                 }, 4000);
+                $('#nota').hide();                  
             }
         }
     });
