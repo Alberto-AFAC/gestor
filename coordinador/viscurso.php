3a64ce67e12cfbd0d67708f242076232226fe192
Alberto-AFAC <jmondragonescamilla@gmail.com>
2022-02-18 14:20:56 -0600

    coordinador

--- coordinador/viscurso.php
+++ coordinador/viscurso.php
@@ -269,12 +269,12 @@
                                 <input type="hidden" name="gstTitulo" id="gstTitulo">
                                 <input type="hidden" name="codigoCurso" id="codigoCurso" />
                                 <!-- notiocu -->
-                                <span id="" data-toggle="modal" data-target="#notificarConv"
+                                <span id="notiocu" data-toggle="modal" data-target="#notificarConv"
                                     style="font-size:12px; width:280px; height:30px "
                                     class="btn btn-info btn-sm altaboton"><i class="fa fa-envelope-open"
                                         aria-hidden="true"></i> NOTIFICAR CONVOCATORIA A PARTICIPANTES</span>
 
-                                <span id="" data-toggle="modal" data-target="#notificarRespon"
+                                <span id="notiocus" data-toggle="modal" data-target="#notificarRespon"
                                     style="font-size:12px; width:280px; height:30px "
                                     class="btn btn-info btn-sm altaboton"><i class="fa fa-graduation-cap"
                                         aria-hidden="true"></i> NOTIFICAR CONVOCATORIA A RESPONSABLES</span>
