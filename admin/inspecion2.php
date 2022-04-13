a1d6697bb7a98678efbbeffe13c831b62870abd5
jessicasoto01afac <jessica.soto928@gmail.com>
2022-04-13 14:10:48 -0500

    DOBLE MODAL EN INSPECCION

--- admin/inspecion2.php
+++ admin/inspecion2.php
@@ -430,6 +430,25 @@ immediately after the control sidebar -->
 </html>
 <link rel="stylesheet" type="text/css" href="../boots/bootstrap/css/select2.css">
 <script type="text/javascript">
+    $(document).on({
+    'show.bs.modal': function() {
+        var zIndex = 1040 + (10 * $('.modal:visible').length);
+        $(this).css('z-index', zIndex);
+        setTimeout(function() {
+            $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass(
+                'modal-stack');
+        }, 0);
+    },
+    'hidden.bs.modal': function() {
+        if ($('.modal:visible').length > 0) {
+            // restore the modal-open class to the body element, so that scrolling works
+            // properly after de-stacking a modal.
+            setTimeout(function() {
+                $(document.body).addClass('modal-open');
+            }, 0);
+        }
+    }
+}, '.modal');
 $(document).ready(function() {
     $('#gstIDSpe').select2();
     $('#gstIDara').select2();
