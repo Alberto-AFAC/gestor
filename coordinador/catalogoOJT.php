4522c6973301b8c560520f2a2e7d35795d3c3bfd
jessicasoto01afac <jessica.soto928@gmail.com>
2022-08-12 12:13:47 -0500

    correción de ortograia

--- coordinador/catalogoOJT.php
+++ coordinador/catalogoOJT.php
@@ -101,7 +101,7 @@ $especialidad = mysqli_query($conexion,$sqlEspecialidad);
 
                         <div class="box">
                             <div class="box-header">
-                                <h3 class="box-title">LISTA DE PROGRMACION DE OJT</h3>
+                                <h3 class="box-title">LISTA DE PROGRAMACION DE OJT</h3>
                                 <div class="pull-right">
 
                                     <div class="btn-group">
